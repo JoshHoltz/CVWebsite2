426b0ef2c68b42db6618fe7b1e0fe37326c1a867
Josh <joshua.holtz@outlook.com>
2024-05-30 23:59:27 +0100

    Trying to work

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ for (const project of projectColumns) {
     });
 }
 
+function toggleProjectTemplate() {
+    let projectTemplate = document.getElementById('projectTemplate');
+    if (projectTemplate.style.display === 'none' || projectTemplate.style.display === '') {
+        projectTemplate.style.display = 'flex';
+        for (const projectTitle of projectColumns) {
+            projectTitle.classList.remove('enlarged');
+        }
+
+    } else {
+        projectTemplate.style.display = 'none';
+    }
+}
+
+function toggleProjectTemplate2() {
+    let projectTemplate = document.getElementById('projectTemplate2');
+    if (projectTemplate.style.display === 'none' || projectTemplate.style.display === '') {
+        projectTemplate.style.display = 'flex';
+        for (const projectTitle of projectColumns) {
+            projectTitle.classList.remove('enlarged');
+        }
+
+    } else {
+        projectTemplate.style.display = 'none';
+    }
+}
+
